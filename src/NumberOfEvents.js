06facad6d30e62626c910eb3b6a2c2619a2121f6
Numenot <qlv6719@gmail.com>
2022-02-14 12:01:16 +0100

    Added warning and error alerts when selecting city and number of events

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
   state = {
-    eventCount: '32'
+    eventCount: '32',
+    errorText: ''
   }
 
   handleInputChanged = (event) => {
     let value = event.target.value;
-    this.setState({
-      eventCount: value
-    });
-    this.props.updateNumberOfEvents(event.target.value);
+    if (value >= 1 && value <= 32) {
+      this.setState({
+        eventCount: value,
+        errorText: ''
+      });
+      this.props.updateNumberOfEvents(event.target.value);
+    }
+    else (
+      this.setState({
+        eventCount: value,
+        errorText: 'Please select a number between 1 and 32'
+      })
+    )
   };
 
   render() {
     return (
       <div className='NumberOfEvents'>
         <label htmlFor='event-number'>Number of events: </label>
+        <ErrorAlert text={this.state.errorText} />
         <input
           name='event-number'
           type='number'
